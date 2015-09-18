c7e71b416eac9e43dad6c52d07d25b76736cfefb
luoluowushengmimi <luoluowushengmimi@163.com>
2015-09-18 17:03:32 +0800

    add alter update

--- sql/sql_plus_study.sql
+++ sql/sql_plus_study.sql
@@ -6,12 +6,13 @@
 create table tab_new like tab_old
 --创建了表的结构的同时也复制了表的内容
 create table order_test_tmp as select * from order_test;
+--创建表时增加主键并且要求它自增
+create table tablename (id not null auto increment,name varchar(20),primary key(id));
+#这样的条件下创建的表应当这样插入数据
+	insert into user_tmp(name,subname) values('qu','guodong');
 
 --修改一张表
 alter table order_test_tmp add column name varchar(10);
-
-
-
 
 --查询一张表
 
@@ -45,11 +46,40 @@ select * from table where column is not null;
 
 delete from table where column='';
 
-
-
---update变换数据
+--update变换数据内容
 
 update table set columnname='' where columnname='';
+
+--更为高级的数据更新工作，例如引入case条件，进行条件判断
+update table 
+	set category=
+	case 
+	when drama='T' then 'drama'
+	when comedy='T' then 'comedy'
+	when action='T' then 'action'
+	when gore='T' then 'gore'
+	when scifi='T' then 'scifi'
+	else 'misc'
+	end;
+
+
+
+--alter修改表的结构
+
+--增加字段
+Alter table tabname add primary key(ID)；
+--删除字段
+Alter table tablename drop primary key(ID)；
+--修改表名
+alter table tablename rename newname;
+--修改表的列的名字和类型
+alter table tablename change column columnname newname int not null ;
+--如果保留列名但是想要修改列的类型时
+alter table tablename modify column int not null ;
+
+
+
+
 
 
 
